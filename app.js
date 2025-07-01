d893b72a80cb4adc10a5c70d2b61ebecc1bff17f
RYOSUE <Ryo.Suetake@jp.ricoh.com>
2025-07-01 17:06:34 +0900

    【rev】preparation for using ejs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
 const app = express();
+const __dirname = import.meta.dirname;
+import path from 'path';
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (req,res) => {
-    res.send('Hello, World!');
-})
+    res.render('../home');
+});
 
 app.listen(3000, () => {
     console.log('ポート3000でリクエストを待ち受け中...');

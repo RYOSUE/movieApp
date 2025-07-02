9292a7538fa9bd1ef8cd9f23c87fc230ad8f280c
RYOSUE <Ryo.Suetake@jp.ricoh.com>
2025-07-02 14:19:22 +0900

    【rev】 make mysql connection an async func

--- app.js
+++ app.js
@@ -6,22 +6,26 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.join(__dirname, '.env') });
 import mysql from 'mysql2/promise';
 
-// MySQLの接続設定
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+async function main() {
+    // MySQLの接続設定
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
 
 
-app.get('/', (req,res) => {
-    res.render('../home');
-});
+    app.get('/', (req, res) => {
+        res.render('../home');
+    });
 
-app.listen(3000, () => {
-    console.log('ポート3000でリクエストを待ち受け中...');
-});
+    app.listen(3000, () => {
+        console.log('ポート3000でリクエストを待ち受け中...');
+    });
+}
+
+main().catch(console.error);

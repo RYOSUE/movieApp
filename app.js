376783845de1c256caeb5711af579bf8b44214ac
RYOSUE <Ryo.Suetake@jp.ricoh.com>
2025-07-02 13:35:54 +0900

    【add】preparation for using mysql in app.js

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ import express from 'express';
 const app = express();
 const __dirname = import.meta.dirname;
 import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.join(__dirname, '.env') });
+import mysql from 'mysql2/promise';
+
+// MySQLの接続設定
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
